c79eca63661989e1016e6779534a9bb49c4a80bb
Dani Vieira <45275551+nyela@users.noreply.github.com>
2019-12-26 23:09:53 -0300

    Lógica de programação em python
    ->Como Calcular a Médica;
    -> Como Calcular o resto;
    -> Como Calcular para mostra o maior nº.

---
+++ 26122019.py
@@ -0,0 +1,49 @@
+#Calculando a nota do bimestre:
+a= int (input('Primeiro bimestre'))
+if a > 10:
+    a = int (input('Você informou a primeira nota errado, por favor verificar  a nota do bimestre:'))
+b= int (input('Segundo bimestre'))
+if b > 10:
+     b= int (input('Você informou a segunda  nota errado, por favor verificar  a nota do bimestre:'))
+c= int (input('Tereseiro  bimestre'))
+if c > 10:
+     c = int (input('Você informou a terceira nota errado, por favor verificar  a nota do bimestre:'))
+d= int (input('Quarto bimestre'))
+ d = int (input('Você informou a quarta nota errado, por favor verificar  a nota do bimestre:'))
+
+media = (a + b+c+d)/4
+#if a <= 10 and b <= 10 and c  <= 10 and d <= 10:
+    print('media : {}'.format(media))
+#else:
+  #  print('Foi onformado alguma nota errada')
+
+  
+#Calcular o nº impar / par:
+
+#a = int (input('Informe um valor:' ))
+#b= int (input('informe o segundo valor:'))
+#resto_a=a % 2
+#resto_b=b%2 
+#if resto_a ==0 or not resto_b >0:
+  #  print('foi informado um Número é par')
+#else:
+    #print('foi informado um Número é impar')
+
+
+
+
+  # Mostrando como se mostrar o maior número:
+  
+#a = int (input ('Primeiro valor: '))
+#b = int  (input ('Segundo valor: '))
+#c = int (input ('Terceiro valor'))
+
+#if a > b and a > c:
+  #  print('O maior Número é {}'.format(a))
+#else:
+#elif b > a and b> c:
+  #  print('O Maior Número é {}'.format(b))
+#else:
+  #  print ('O Maior Número é {}'.format(c))
+#print ('Final do Programa')
+
